6e39b9f93511e61431e6b606043bdd9a199a791c
shubham maithani <shubhammaithani1@gmail.com>
2022-09-14 00:07:53 -0700

    Part time emp and Wage

--- Employeewage.java
+++ Employeewage.java
@@ -3,21 +3,21 @@ package com.day3;
 public class Employeewage {
 
 	public static void main(String[] args) {
-		// constant
-		int Full_Time = 1;
-		int Emp_Rate_per_Hr = 20;
-		// variables
-		int empHrs = 0;
-		int empWadge = 0;
+		int Part_Time = 1;
+		int Full_Time = 2;
+		int Emp_Rate_per_Hrs = 20;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == Full_Time)
+		int empHrs = 0;
+		int empWedge = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Part_Time)
+			empHrs = 4;
+		else if (empCheck == Full_Time)
 			empHrs = 8;
 		else
-
 			empHrs = 0;
-		empWadge = empHrs * Emp_Rate_per_Hr;
-		System.out.println("emp wedge: " + empWadge);
+		empWedge = empHrs * Emp_Rate_per_Hrs;
+		System.out.println("Emp Wdge:" + empWedge);
 
 	}
 }
