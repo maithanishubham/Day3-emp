f59c93d0b54ef69bbf7a0ffbe8413694f0cf8c91
shubham maithani <shubhammaithani1@gmail.com>
2022-09-14 00:02:44 -0700

    Employee daily wage

--- Employeewage.java
+++ Employeewage.java
@@ -3,15 +3,21 @@ package com.day3;
 public class Employeewage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Attendence");
+		// constant
+		int Full_Time = 1;
+		int Emp_Rate_per_Hr = 20;
+		// variables
+		int empHrs = 0;
+		int empWadge = 0;
 
-		int Attendance = 1;
-		double randomCheck = (Math.floor(Math.random() * 10)) % 2;
-		int check = 0;
-		if (check == Attendance) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is Absent");
-		}
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Full_Time)
+			empHrs = 8;
+		else
+
+			empHrs = 0;
+		empWadge = empHrs * Emp_Rate_per_Hr;
+		System.out.println("emp wedge: " + empWadge);
+
 	}
 }
