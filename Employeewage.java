aff764d4745519cb5f72158083735361fc912b7e
shubham maithani <shubhammaithani1@gmail.com>
2022-09-18 04:22:36 -0700

    Use Class method and variable

--- Employeewage.java
+++ Employeewage.java
@@ -2,38 +2,32 @@ package com.day3;
 
 public class Employeewage {
 
-	public static final int Part_time = 1;
-	public static final int Full_time = 2;
-	public static final int Emp_rate_per_Hrs = 20;
-	public static final int Num_of_days_working = 5;
-	public static final int Max_Hrs_In_Month = 10;
-	private static final int Num_of_Working_Days = 0;
+	public static void main(String args[]) {
 
-	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Attendance");
 
-		System.out.println("Welcome to Employee wage computation program");
-
-		int empHrs = 0;
-		int totalEmpHrs = 0;
-		int totalworkingDays = 0;
-
-		while (totalEmpHrs <= Max_Hrs_In_Month && totalworkingDays < Num_of_Working_Days) {
-			totalworkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-			case Part_time:
-				empHrs = 4;
-				break;
-			case Full_time:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
-			}
-			totalEmpHrs += empHrs;
-			System.out.println("Day#:" + totalworkingDays + "Emp Hr: " + empHrs);
-		}
-		int totalEmpwage = totalEmpHrs * Emp_rate_per_Hrs;
-		System.out.println("Total Emp wage:" + totalEmpwage);
+		Employeewage Wage = new Employeewage();
+		Wage.Employee();
 	}
+
+	public void Employee() {
+		int noOfhourperday = 8;
+		int Wageperhr = 20;
+		int Attendance = 1;
+		double randomCheck = (Math.floor(Math.random() * 10)) % 2;
+		if (Attendance == randomCheck) {
+
+			if (noOfhourperday == 8) {
+				System.out.println("Employee is present");
+				int totalWage = noOfhourperday * Wageperhr;
+				System.out.println("wage per day is:" + totalWage);
+
+			}
+		}
+
+		else {
+			System.out.println(" Employee is Absent");
+		}
+	}
+
 }
